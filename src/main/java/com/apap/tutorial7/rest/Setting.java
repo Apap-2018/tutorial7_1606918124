2d2dfe7653bb0c05ada5a5929b695d8cd099beea
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-11-01 20:26:36 +0700

    Done All Tutorial + Latihan

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,4 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String pilotUrl = "https://53e6c667-c8c4-41ee-b31c-29ef8b2d6b56.mock.pstmn.io";
+	final public static String airportUrl = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=OZDTmaBlgyJxUg1xTWM9nNQKCAMWCdsh&term=";
 }
